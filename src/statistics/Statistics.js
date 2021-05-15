7cb41c208d468cdccff5b5f799aa9ad53c0ffb71
DM-MRTNVCH <dmvshn@gmail.com>
2021-05-15 23:21:36 +0300

    firebase deploy

---
+++ src/statistics/Statistics.js
@@ -0,0 +1,38 @@
+import React, {useContext, useEffect} from 'react';
+import styles from './Statistics.module.css'
+import {Doughnut} from 'react-chartjs-2';
+import {useDispatch, useSelector} from 'react-redux';
+import {loadStatisticsAC} from './statistics-actions'
+import {ageStatistics, genderStatistics, levelStatistics} from './statistics-presenter'
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+import {Context} from '../index';
+
+function Statistics() {
+
+    const {firestore} = useContext(Context);
+    const [data, loading] = useCollectionData(
+        firestore.collection('data')
+    )
+
+    if (loading) {
+        return 'LOADING';
+    }
+
+    return (
+        <div className={styles.wrap}>
+            <div className={styles.statisticsContainer}>
+                <div className={styles.eachDoughnut}>
+                    <Doughnut data={ageStatistics(data)}/>
+                </div>
+                <div className={styles.eachDoughnut}>
+                    <Doughnut data={genderStatistics(data)}/>
+                </div>
+                <div className={styles.eachDoughnut}>
+                    <Doughnut data={levelStatistics(data)}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Statistics
